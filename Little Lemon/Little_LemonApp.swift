9d9598f832d8c090383d7caf6d32d9980e3bbd48
Amine Boularbah <boularbah.amine2000@gmail.com>
2024-11-20 18:57:44 +0100

    Initial Commit

---
+++ Little Lemon/Little_LemonApp.swift
@@ -0,0 +1,20 @@
+//
+//  Little_LemonApp.swift
+//  Little Lemon
+//
+//  Created by Amine on 20/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct Little_LemonApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
